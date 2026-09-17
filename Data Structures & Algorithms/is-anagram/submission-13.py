903921d823e0d6258a16c3173b4cd18d53405f98
Sean Dhanda <72596711+seandhanda@users.noreply.github.com>
2026-09-16 22:24:41 -0700

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+
+        if len(s) != len(t):
+            return False
+
+        s_hashmap = {}
+        t_hashmap = {}
+
+        for i in range(len(s)):
+            s_hashmap[s[i]] = s_hashmap.get(s[i],0)+1
+            t_hashmap[t[i]] = t_hashmap.get(t[i],0)+1
+
+        if s_hashmap != t_hashmap:
+            return False
+        
+        return True
