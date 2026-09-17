af02cb966a71041991ad63227c2dea97dcea7c2d
Sean Dhanda <72596711+seandhanda@users.noreply.github.com>
2026-09-17 16:55:48 -0700

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # #Brute force iterate across every index for every index
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             return [i,j]
+        # # O(n^2)
+        # # Space: O(1)
+        
+        my_map = {} # val : index
+
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in my_map:
+                return [my_map[diff], i]
+            my_map[n] = i
+        
+        return
