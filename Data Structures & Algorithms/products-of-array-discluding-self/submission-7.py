bfcd5d6081b9edbe35608a1a1e236d86a58e44a7
Sean Dhanda <72596711+seandhanda@users.noreply.github.com>
2026-09-20 23:10:47 -0700

    Add: products-of-array-discluding-self - submission-7

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-7.py
@@ -0,0 +1,39 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        # Optimal = 2 Passes Prefix and Postfix and then one pass calculation all in output 
+
+        solution = [0] * len(nums)
+
+        prefix = 1
+        for n in range(len(nums)):
+            solution[n] = prefix
+            prefix *= nums[n] 
+        # for n in range(len(nums)-1):
+        #     solution[n] *= solution[n+1]*nums[n+1]
+        
+        postfix = 1
+        for n in range(len(nums)-1, -1,-1):
+            solution[n] *= postfix
+            postfix *= nums[n] 
+
+        return solution
+
+        # O(n)
+        #O(1) space
+
+
+
+        # #BRUTE FORCE:
+        # output = []
+
+        # for i in range(len(nums)):
+        #     subsolution = 1
+        #     for j in range(len(nums)):
+        #         if j != i:
+        #             subsolution *= nums[j]
+        #     output.append(subsolution)
+        # return output
+
+        # #O(n^2)
+        # # Space: O(n)
