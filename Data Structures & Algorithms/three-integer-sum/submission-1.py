fb0502383ec580102f7aee7a9b86765d111e3d44
Sean Dhanda <72596711+seandhanda@users.noreply.github.com>
2026-09-24 16:12:09 -0700

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,51 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Optimal:
+
+        solution = []
+
+        nums.sort()
+
+        for i in range(len(nums)-2):
+            if nums[i] == nums[i-1] and i>0:
+                continue
+            target = -nums[i] 
+            left = i+1
+            right = len(nums)-1
+
+            while left < right:
+                if nums[left] + nums[right] == target:
+                    if [nums[i], nums[left], nums[right]] not in solution:
+                        solution.append([nums[i], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        continue
+                    elif [nums[i], nums[left], nums[right]] in solution:
+                        left += 1
+                        right -= 1
+                        continue
+                elif nums[left] + nums[right] < target:
+                    left += 1
+                    continue
+                elif nums[left] + nums[right] > target:
+                    right -= 1
+                    continue
+
+        return solution
+        # O(n^2)time
+        #O(1) space
+
+        
+        # #brute force - just iterate
+        # solution = []
+
+        # for i in range(len(nums)-2):
+        #     for j in range(i+1, len(nums)-1,+1):
+        #         for k in range(j+1, len(nums),+1):
+        #             if nums[i] + nums[j] + nums[k] == 0:
+        #                 if sorted([nums[i],nums[j],nums[k]]) not in solution:
+        #                     solution.append(sorted([nums[i], nums[j], nums[k]]))
+        # return solution
+
+        # #O(1) space
+        # #O(n^3) time
