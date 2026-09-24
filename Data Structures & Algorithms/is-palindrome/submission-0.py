ebadd415126ccb72fb0328d998db776585d806e6
Sean Dhanda <72596711+seandhanda@users.noreply.github.com>
2026-09-23 21:41:00 -0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        
+        left = 0
+        right = len(s)-1
+
+        while left < right: # if even, this is perfect. if odd, the middle character will always pass palindrome requirement. this works!
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if not s[right].isalnum():
+                right -= 1
+                continue
+            
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        
+        return True
+
+        #O(n) time
+        #O(1) space
+        
