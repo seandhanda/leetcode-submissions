2b6dad23440ea2061f88be7c5c1da103ad51e466
Sean Dhanda <72596711+seandhanda@users.noreply.github.com>
2026-09-23 23:27:14 -0700

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        #Optimal
+
+        
+        #not sorted enumerate approach
+
+        lookupMap = defaultdict(int)
+
+        for i,n in enumerate(numbers):
+            lookup = target - n
+            if lookup in lookupMap:
+                return [lookupMap[lookup]+1, i+1]
+            lookupMap[n] = i 
+        
+        # #brute force iterate
+        # for i in range(len(numbers)):
+        #     for j in range(i+1, len(numbers),+1):
+        #         if numbers[i] + numbers[j] == target:
+        #             return [i+1, j+1]
+
+        # #O(n^2)
+        # #O(1) space
+
+
