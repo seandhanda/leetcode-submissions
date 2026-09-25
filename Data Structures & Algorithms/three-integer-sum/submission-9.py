156930064accc3f8e557dc9d64d38ef16f30f40b
Sean Dhanda <72596711+seandhanda@users.noreply.github.com>
2026-09-24 17:25:36 -0700

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.py
@@ -0,0 +1,49 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Optimal:
+        # what if after you find a solution to append, and iterate left pointer, you end up later in a scenario where you get the same solution but with right value and left value switched - its not possible given the algorithm - but how are you expected to know this special cae during interview without memorizing the specific solution?
+
+        solution = []
+
+        nums.sort()
+
+        for i,n in enumerate(nums):
+            if i>0 and n == nums[i-1]:
+                continue
+        
+            left = i+1
+            right = len(nums)-1
+            target = -n
+
+            while left < right:
+                if nums[left] + nums[right] < target:
+                    left += 1
+                    continue
+                elif nums[left] + nums[right] > target:
+                    right -= 1
+                    continue
+                elif nums[left] + nums[right] == target:
+                    solution.append([n, nums[left], nums[right]])
+                    left += 1
+                    while left <= len(nums)-1 and nums[left] == nums[left-1]:
+                        left +=1
+                    continue
+        return solution
+        # O(n^2)time
+        #O(1) space
+        # WE DO NOT WANT if not in solution, this can be O(n^3) triples in solution, so O(n^4) runtime due ot searching solution list in worst case.
+
+        
+        # #brute force - just iterate
+        # solution = []
+
+        # for i in range(len(nums)-2):
+        #     for j in range(i+1, len(nums)-1,+1):
+        #         for k in range(j+1, len(nums),+1):
+        #             if nums[i] + nums[j] + nums[k] == 0:
+        #                 if sorted([nums[i],nums[j],nums[k]]) not in solution:
+        #                     solution.append(sorted([nums[i], nums[j], nums[k]]))
+        # return solution
+
+        # #O(1) space
+        # #O(n^3) time
